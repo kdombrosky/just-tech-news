2701df997789bc0bfcefc587a193edb86bfdff44
Kayla Dombrosky <kdombroskyy@live.com>
2021-07-15 18:24:08 -0400

    create api routes for post model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,20 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations 
+// creates reference for the id column in the User model to link to the 
+// coresponding foreign key pair, user_id in Post model
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// make reverse association
+// define relationship of Post model to User
+// constraint here means a post can belong to one user, but not many users
+// then declare link to foreign key
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+
 module.exports = { User, Post };
