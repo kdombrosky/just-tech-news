36baeb3c30bb442dd98911b3139f1f8b2a3386bd
Kayla Dombrosky <kdombroskyy@live.com>
2021-07-15 17:44:09 -0400

    create the post model

---
+++ models/Post.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our Post model
+class Post extends Model {}
+
+// create fields/columns for Post model
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // Sequelize's ability to offer validation in the schema definition
+                isURL: true
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,   
+            // establish relationship between specific post and user 
+            // by referencing the User model, specifically to the primary key id 
+            // so user_id is defined as the foreign key 
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
